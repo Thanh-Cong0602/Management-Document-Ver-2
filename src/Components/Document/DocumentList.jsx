493567ee06b53bb9f0a472748ae1e1b020295707
quangkhai482002 <118819349+quangkhai482002@users.noreply.github.com>
2023-11-26 11:45:25 +0700

    delete

---
+++ src/Components/Document/DocumentList.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
+import { UploadOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Button, message, Upload } from "antd";
+import axios from "axios";
+
+const columns = [
+  {
+    title: "STT",
+    dataIndex: "index",
+    key: "index",
+  },
+  {
+    title: "Tên",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Mô tả",
+    dataIndex: "description",
+    key: "description",
+  },
+  {
+    title: "URL",
+    dataIndex: "filename",
+    key: "filename",
+    render: (text, record) => (
+      <a href={record.url} target="__blank">
+        {text}
+      </a>
+    ),
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
+    key: "action",
+    render: (text, record) => (
+      <div
+        type="button"
+        style={{ color: "red" }}
+        // onClick={() => {
+        //   axios.delete(`http://localhost/document/${record.id}`).then(() => {
+        //     window.location.reload();
+        //   });
+        // }}
+      >
+        <DeleteOutlined />
+      </div>
+    ),
+  },
+];
+
+const props = {
+  name: "file",
+  action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+const DocumentList = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost/document").then((res) => {
+      const resData = res.data.map((item, key) => {
+        return {
+          ...item,
+          index: key + 1,
+        };
+      });
+      setData(resData);
+      console.log(resData);
+    });
+  }, []);
+  return (
+    <div>
+      <Table
+        columns={columns}
+        bordered
+        title={() => "Danh sách"}
+        dataSource={data}
+      />
+    </div>
+  );
+};
+export default DocumentList;
