74f87bcff8814efc501689e2254ba3c69d96a076
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2023-11-26 11:02:12 +0700

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import UpdateInforUser from "./Components/User/UpdateInforUser/UpdateInforUser";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -14,5 +10,6 @@ function App() {
     </>
   );
 
+}
 
 export default App;
