25c599ecea1ec6fab3080a56a1868645e4b6ec12
nghiaduong2202 <nghia.duong2202@gmail.com>
2023-12-08 19:30:43 +0700

    update role

--- src/Components/Admin/ManageAccounts/ManageAccount.jsx
+++ src/Components/Admin/ManageAccounts/ManageAccount.jsx
@@ -3,79 +3,81 @@ import { Table } from "antd";
 import { useEffect, useState } from "react";
 import { getAllUsers, updateRole } from "../../../Api/Service/user.service";
 
-const columns = [
-  {
-    title: "STT",
-    dataIndex: "stt",
-    key: "stt",
-  },
-  {
-    title: "Tên",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Vai trò",
-    dataIndex: "role",
-    key: "role",
-    render: (record) => <div>{record.role}</div>,
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Giới tính",
-    dataIndex: "gender",
-    key: "gender",
-    render: (text) => <div>{text ? "Nam" : "Nữ"}</div>,
-  },
-  {
-    title: "Ngày sinh",
-    dataIndex: "dob",
-    key: "dob",
-  },
-  {
-    title: "Điện thoại",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Action",
-    dataIndex: "action",
-    key: "action",
-    render: (_, record) => (
-      <div style={{ display: "Flex" }}>
-        <div
-          type="button"
-          style={{ color: "red" }}
-          //onClick={() => {
-          //  axios.delete(`http://localhost/user/${record.id}`).then(() => {
-          //    window.location.reload();
-          //  });
-          //}}
-        >
-          <DeleteOutlined />
-        </div>
-        <div
-          type="button"
-          style={{ color: "green", marginLeft: "30%" }}
-          onClick={() => {
-            updateRole(
-              `user/${record.id}/role?role=${
-                record.role.role === "user" ? "admin" : "user"
-              }`
-            ).then();
-          }}>
-          <RetweetOutlined />
-        </div>
-      </div>
-    ),
-  },
-];
-
 const ManageAccount = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "stt",
+      key: "stt",
+    },
+    {
+      title: "Tên",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Vai trò",
+      dataIndex: "role",
+      key: "role",
+      render: (record) => <div>{record.role}</div>,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Giới tính",
+      dataIndex: "gender",
+      key: "gender",
+      render: (text) => <div>{text ? "Nam" : "Nữ"}</div>,
+    },
+    {
+      title: "Ngày sinh",
+      dataIndex: "dob",
+      key: "dob",
+    },
+    {
+      title: "Điện thoại",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      render: (_, record) => (
+        <div style={{ display: "Flex" }}>
+          <div
+            type="button"
+            style={{ color: "red" }}
+            //onClick={() => {
+            //  axios.delete(`http://localhost/user/${record.id}`).then(() => {
+            //    window.location.reload();
+            //  });
+            //}}
+          >
+            <DeleteOutlined />
+          </div>
+          <div
+            type="button"
+            style={{ color: "green", marginLeft: "30%" }}
+            onClick={() => {
+              updateRole(
+                `user/${record.id}/role?role=${
+                  record.role.role === "user" ? "admin" : "user"
+                }`
+              ).then(() => {
+                setIsLoading(!isLoading);
+              });
+            }}>
+            <RetweetOutlined />
+          </div>
+        </div>
+      ),
+    },
+  ];
   const [data, setData] = useState([]);
   useEffect(() => {
     getAllUsers("user").then((res) => {
@@ -88,7 +90,7 @@ const ManageAccount = () => {
       setData(resData);
       // console.log(resData);
     });
-  }, [data]);
+  }, [isLoading]);
   return (
     <div>
       <Table
